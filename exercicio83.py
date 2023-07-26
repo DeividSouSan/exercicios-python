aee9e30a45c710da9ff37f8f96993405954a6514
Deivid Souza Santana <deividsantana2013@gmail.com>
2023-07-26 17:05:26 -0300

    add: exercício 83 completo

--- exercicio83.py
+++ exercicio83.py
@@ -7,27 +7,27 @@ Crie um programa que o usuário digite uma expressão qualquer que use parentese
 #'((a + b) * c)(x + y(3 + 2 / 3) * z)'
 #'((((a + b) * c) + 2) / 4))'
 #'a + b ) * c ('
-# str(input("Digite uma expressão: "))
-brackets = list()
-expr = '((a + b) * c)(x + y(3 + 2 / 3) * z)'
 
-for char in expr:
-    if char in '()':
-        brackets.append(char)
+bracketStack = list()
+expr = str(input("Digite uma expressão: "))
 
-openBrackets = brackets[:len(brackets) // 2]
-closedBrackets = brackets[len(brackets) // 2:]
+for symbol in expr:
+    if symbol == '(':
+        bracketStack.append('(')
+    elif symbol == ')':
+        if len(bracketStack) > 0:
+            bracketStack.pop()
+        else:
+            bracketStack.append(')')
 
-if len(openBrackets) == len(closedBrackets):
-    if ')' in openBrackets or '(' in closedBrackets:
-        print("A expressão não é válida")
-    else:
-        print("A expressão é válida.")
+if len(bracketStack) == 0:
+    print("A expressão é válida.")
 else:
-    print("A expressãon não é válida.")
+    print("A expressão não é válida devido aos símbolos sem pares: ", bracketStack)
+    
+"""
+Cada caractere da pilha vai ser percorrido, quando o caracterefor ( ele é adicionado na pilha, se o símbolo encontrado for ) ele verifica se tem algo na pilha, se houver então ele remove o último ( adiionado, caso não haja nada significa que aquele ( não tem par.
+""" 
 
-print(brackets)
-print(openBrackets)
-print(closedBrackets)
 
 
